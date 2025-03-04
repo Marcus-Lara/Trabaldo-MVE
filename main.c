c2aeb19887fbc56bef0b0488af8df7802daed56d
NivaJr <129238856+NivaJr@users.noreply.github.com>
2025-03-04 09:48:51 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
+int opcao;
+typedef struct{
+    int codigo_aluno;
+    char nome[100];
+    char biotipo[25];
 
-// menu de entrada
-void bem_vindo(){
-    printf("********** BEM VINDO A MAROMBIME **********\n\n\n");
-    printf("********** O QUE DESEJAS? *****************\n 0 - CADASTRAR;\n 1 - MONTAR TREINO.\n\n");
+}Aluno;
+typedef struct{
+    int codigo_treino;
+    char nome_treino[25];
+    char nivel[25];
+    int duracao;
+}Treino;
+//procedimento para imprimir dados do aluno
+void imprimir_aluno(Aluno al){
+    printf("\nCodigo de Matricula: %d", al.codigo_aluno);
+    printf("\nNome Completo: %s", al.nome);
+    printf("\nBiotipo:%s", al.biotipo);
+}
+//Função que le os dados do aluno
+Aluno ler_aluno(){
+    Aluno al;
+    printf("\nDigite seu codigo: ");
+    scanf("%d", &al.codigo_aluno);
+    scanf("%c");
+    printf("\nDigite seu nome completo: ", al.nome);
+    scanf("%[^\n]", al.nome);
+    scanf("%c");
+    printf("\nDigite seu biotipo: ", al.biotipo);
+    scanf("%[^\n]", al.biotipo);
+    return al;
+}
+//Função MENU
+int menu(){
+    printf("\n\tMENU:\t\n1-Cadastrar aluno\t\n2-Cadastrar treino\t\n3-Remover aluno\t\n4-Remover treino\t\n5-Consultar alunos\t\n6-Consultar treinos\t\n7-Sair");
+    printf("\nDigite uma das opcoes acima:");
+    scanf("%d", &opcao);
+    return opcao;
 }
 
 int main(){
-    int i;
-    bem_vindo();
-    scanf("%d", &i);
-        switch (i)
+    Aluno al;
+    int opcao;
+    printf("*** ACADEMIA - COMPUTACAO IME 27***");
+    do{
+        opcao = menu();
+        switch (opcao)
         {
         case 1:
-            printf("Vamos precisar dos seus dados:\n");
+            /* code */
             break;
-        case 0:
-            printf("Qual seu objetivo\n");
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;
+        case 4:
+            /* code */
+            break;
+        case 5:
+            /* code */
+            break;
+        case 6:
+            /* code */
             break;
         default:
-            printf("Digite 0 para cadastrar e 1 para montar o treino\n");
+        printf("Opcao invalida!");
             break;
         }
+    }while (opcao != 7);
+    al = ler_aluno(); //variavel auxiliar para receber os valores lidos pela função
+    imprimir_aluno(al);
     return 0;
 }
